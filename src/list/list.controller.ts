c6f7079e99b2ac83b642a615c860d61fb612c05d
Adson N Tanajura <adson.tanajura@gmail.com>
2025-07-25 20:15:26 -0300

    feat(list): protects list routes with JWT authentication

--- src/list/list.controller.ts
+++ src/list/list.controller.ts
@@ -6,17 +6,21 @@ import {
   Param,
   Post,
   Put,
+  UseGuards,
+  Request,
 } from '@nestjs/common';
 import { ListService } from './list.service';
 import { CreateToDoDto } from 'src/dto/create-to-do.dto';
+import { AuthGuard } from '@nestjs/passport';
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('list')
 export class ListController {
   constructor(private readonly listService: ListService) {}
 
   @Post('create')
-  createList(@Body() { title, description }: CreateToDoDto) {
-    return this.listService.createList({ title, description });
+  createList(@Body() createToDoDto: CreateToDoDto, @Request() req) {
+    return this.listService.createList(createToDoDto, req.user.userId);
   }
 
   @Put(':id')
@@ -33,16 +37,17 @@ export class ListController {
   }
 
   @Get('all')
-  getAllLists() {
-    return this.listService.getAllLists();
+  getAllListsByUser(@Request() req) {
+    return this.listService.getAllListsByUser(req.user.userId);
   }
+
   @Get(':id')
   getListById(@Param('id') id: string) {
     return this.listService.getListById(id);
   }
 
-  @Get('search/:term')
+  /* @Get('search/:term')
   searchByTitle(@Param('term') term: string) {
     return this.listService.searchByTitle(term);
-  }
+  } */
 }
