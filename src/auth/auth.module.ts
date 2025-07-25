27761a62a7a986abb7a4e5144dbb9feaea5ef63b
Adson N Tanajura <adson.tanajura@gmail.com>
2025-07-25 20:07:08 -0300

    feat(Implement JWT authentication): Adds auth.service and auth.module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { UserModule } from 'src/user/user.module';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/entity/user.entity';
 
 @Module({
   imports: [
     UserModule,
     PassportModule,
+    TypeOrmModule.forFeature([User]),
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'defaultSecret',
-      signOptions: { expiresIn: '1d' }, // Token expiration time
+      signOptions: { expiresIn: process.env.JWT_EXPIRES_IN || '1h' },
     }),
   ],
   exports: [AuthService],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,24 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { UserService } from 'src/user/user.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { LoginUserDto } from 'src/dto/login-user.dto';
+import { User } from 'src/entity/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthService {
   constructor(
-    private userService: UserService,
+    @InjectRepository(User) private userRepository: Repository<User>,
     private jwtService: JwtService
   ) {}
 
-  async validateUser(email: string, password: string): Promise<any> {
-    const user = await this.userService.loginUser({ email, password });
+  async validateUser({ email, password }: LoginUserDto): Promise<any> {
+    const user = await this.userRepository.findOne({
+      where: { email: email },
+      select: ['id', 'email', 'password'],
+    });
+
+    if (!user || user.password !== password) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
     if (user) {
       const { password, ...result } = user;
       return result;
     }
-    return null;
+
+    throw new UnauthorizedException('Invalid credentials');
   }
 
-  async login(user: any) {
+  async login(user: User) {
     const payload = { email: user.email, sub: user.id };
     return {
       access_token: this.jwtService.sign(payload),
