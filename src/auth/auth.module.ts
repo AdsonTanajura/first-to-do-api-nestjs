7223316c1ae08ae3175d6fd014c867f557baa62d
Adson N Tanajura <adson.tanajura@gmail.com>
2025-07-31 19:12:15 -0300

    fix(auth): fixes the problem with the jwt check

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,15 +7,26 @@ import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/user/entity/user.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UserModule,
     PassportModule,
+    ConfigModule,
+
     TypeOrmModule.forFeature([User]),
-    JwtModule.register({
-      secret: process.env.JWT_SECRET || 'defaultSecret',
-      signOptions: { expiresIn: process.env.JWT_EXPIRES_IN || '1h' },
+
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_EXPIRATION_TIME') || '1h',
+        },
+      }),
     }),
   ],
   exports: [AuthService],
